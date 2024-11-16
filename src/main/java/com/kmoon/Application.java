b08f8685a28586567a0c05684efaa654277dcad9
Kmoon <hushan@kmoon.fun>
2024-11-16 15:57:53 +0800

    Update Application.java

--- src/main/java/com/kmoon/Application.java
+++ src/main/java/com/kmoon/Application.java
@@ -5,14 +5,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-/**
- * 代码千万行，
- * 注释第一行。
- * 代码没注释，
- * 维护两行泪。
- */
-
 @MapperScan(basePackages = "com.kmoon.mapper")
 @SpringBootApplication
 public class Application {
